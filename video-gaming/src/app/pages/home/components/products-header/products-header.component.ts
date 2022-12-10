0bd3fc8a24caf71b6739b3a1e5b172741a7adbac
Nikoletazl <nikoleta_zlateva02@abv.bg>
2022-12-10 22:36:34 +0200

    structure and components

---
+++ video-gaming/src/app/pages/home/components/products-header/products-header.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-products-header',
+  templateUrl: './products-header.component.html',
+  styleUrls: ['./products-header.component.css']
+})
+export class ProductsHeaderComponent implements OnInit {
+  sort = 'desc'
+  itemsShowCount = 12
+  @Output() columnsCountChange = new EventEmitter<number>()
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSortUpdated(newSort: string): void {
+    this.sort = newSort
+  }
+
+  onItemsUpdated(count: number): void {
+    this.itemsShowCount = count
+  }
+
+  onColumnsUpdated(colNum: number): void {
+    this.columnsCountChange.emit(colNum)
+  }
+
+}
